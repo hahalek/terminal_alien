c1b4840c52286c4a246849e92316f3a280430d7d
hahalek <agata.machalek@gmail.com>
2026-04-25 17:53:36 +0200

    Added shrimp sprite (ascii art). Function to print it at specified x, y coordinates facing left and right.

---
+++ idea.md
@@ -0,0 +1,17 @@
+Terminal-based taamagochi like game/simulation
+
+Basic features:
+- [ ] There is a creature that is displayed in the terminal
+- [ ] The creature can move freely inside the terminal window
+- [ ] The creature sometime moves to random points
+- [ ] Sometimes food spawns inside the terminal
+- [ ] Creature goes and eats food that has spawned
+- [ ] Sometimes creature goes to sleep and wakes up
+- [ ] State of the simulation can be saved to a file and loaded from it
+
+
+Additional ideas:
+- [ ] Creature has needs: Hunger, Sleep, Fun
+- [ ] Creature statistics are displayed in the terminal
+- [ ] Creature can say things using a LLM running locally
+- [ ] Creature has more complex moving pattern (not only in straight lines)
---
+++ src/main.py
@@ -0,0 +1,81 @@
+from time import sleep
+from blessed import Terminal
+
+
+term = Terminal()
+
+# # reading keystrokes from terminal
+# term = Terminal()
+# with term.cbreak():
+#     key = term.inkey()
+#     print(f"You pressed: {key!r}")
+
+
+# shrimp_string = r"""
+#     ______________
+#     ____________  \
+#                 \ |
+#                 / /
+#      /=========== * ===
+#     /=============-----
+#    /=============\\
+#   // |||| }}\\\\
+#   |||
+#    \\\
+#     \\\ """
+
+class Shrimp():
+    def __init__(self, sprite_string: str) -> None:
+        self.sprite = sprite_string
+        self.position_x = 0
+        self.position_y = 0
+        
+    def print_at(self, x: int, y: int, wait: float = 0.3) -> None:
+        print(term.clear, end='')
+        for line in self.sprite.splitlines():
+            print(term.move_xy(x, y), ' '*x + line, end='')
+            y += 1
+        print(sleep(wait))
+        
+
+    def print_at_reverse(self, x: int, y: int, wait: float = 0.3)  -> None:
+        print(term.clear, end=' ')
+        for line in self.sprite.splitlines():
+            new_line = ""
+            for char in line:
+                if char == r"/":
+                    new_line += "\\"
+                elif char == "\\":
+                    new_line += r"/"
+                elif char == r"}":
+                    new_line += r"{"
+                else:
+                    new_line += char
+                
+            print(term.move_xy(x, y), ' '*x + new_line[::-1], end=' ')
+            y += 1
+        print(sleep(wait))
+
+
+sprite = r"""A
+  ______________      
+  ____________  \     
+              \ |     
+              / /     
+   /=========== * === 
+  /=============----- 
+ /=============\\     
+// |||| }}\\\\        
+|||                   
+ \\\                  
+  \\\                 """
+
+shrimp = Shrimp(sprite)
+shrimp.print_at_reverse(0, 0,  2)
+shrimp.print_at_reverse(5, 5, 10)
+shrimp.print_at_reverse(10, 10, 10)
+shrimp.print_at(15, 15)
+shrimp.print_at(20, 14, 2)
+shrimp.print_at(23, 18)
+shrimp.print_at(28, 16)
+shrimp.print_at(32, 19)
